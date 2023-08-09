fe714a6321c81946840b36cb0aa875cc1d0a02cd
mahavirskeps <mahavir.goyal@skeps.com>
2023-08-09 15:36:26 +0530

    midlleware multer file and indexjs file updated and working fully

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,18 @@
-const express = require('express')
+const multer=require('multer')
+const path=require('path')
 
-const app = express()
-const port = 3000;
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        console.log("req",req);
+        console.log("reqfile",file);
+        cb(null, '/Users/utkarsh.nigam/Desktop/NodeJS Practice/POC-data-Extractor/service_data_extractor/backend/src/uploads');
+        console.log("hello");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
 
-app.listen(port, ()=>{
-    console.log(`Server is Running on port ${port}`);
-})
+module.exports=upload
--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -1,3 +1,18 @@
-const upload = ("");
+const multer=require('multer')
+const path=require('path')
 
-module.exports = upload;
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        console.log("req",req);
+        console.log("reqfile",file);
+        cb(null, '/Users/utkarsh.nigam/Desktop/NodeJS Practice/POC-data-Extractor/service_data_extractor/backend/src/uploads');
+        console.log("hello");
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, uniqueSuffix + path.extname(file.originalname));
+    }
+});
+const upload = multer({ storage: storage });
+
+module.exports=upload
