78dac86637075df1c020a75d1544fe660b8614d0
mahavirskeps <mahavir.goyal@skeps.com>
2023-08-09 17:37:07 +0530

    commit pr review changes

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -3,8 +3,7 @@ const path=require('path')
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, '/Users/utkarsh.nigam/Desktop/NodeJS Practice/POC-data-Extractor/service_data_extractor/backend/src/uploads');
-        console.log("hello");
+        cb(null, './src/uploads');
     },
     filename: (req, file, cb) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
