0ce5ae741ab76844740d3e0b2f17fdccd3c11550
UtkarshNigamSkeps <utkarsh.nigam@skeps.com>
2023-08-09 16:55:36 +0530

    pr review changes done

--- backend/src/routes/route.js
+++ backend/src/routes/route.js
@@ -3,11 +3,11 @@ const upload=require('../middleware/multer')
 const processController= require('../controllers/process')
 const router=new express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
-});
+// router.get('/', (req, res) => {
+//     res.render('index');
+// });
 
-router.post('/process', upload.single('pdfFile'), processController.processPdf);ß
+router.post('/process', upload.single('pdfFile'), processController.processPdf);
 
 
 module.exports=router
