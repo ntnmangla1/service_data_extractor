a40455b17d5a5600cfaf04e0b2c445dcf00d1293
UtkarshNigamSkeps <utkarsh.nigam@skeps.com>
2023-08-09 17:22:41 +0530

    pr review changes done

--- backend/src/controllers/process.js
+++ backend/src/controllers/process.js
@@ -4,47 +4,47 @@ const pdf = require('pdf-parse');
 const runOcrCommand = require('../utility/ocr'); // Import your OCR function
 
 async function processPdf(req, res) {
-   const inputFile = req.file.path;
-   const outputFile = 'output.pdf'; // Replace with your output file path
-    try{
+    const inputFile = req.file.path;
+    const outputFile = 'output.pdf'; // Replace with your output file path
+    try {
         await runOcrCommand(inputFile, outputFile).then(() => {
             const pdfFilePath = 'output.pdf'; // Replace with your PDF file path
-        
+
             // Read the PDF file
             const pdfData = fs.readFileSync(pdfFilePath);
-        
+
             // Parse the PDF data
             pdf(pdfData).then(data => {
-              const extractedText = data.text;
-        
-              // Now you can search for specific data in the extracted text
-              const searchTerm = req.body.searchTerm; // Replace with the data you want to find
-        
-              if (extractedText.includes(searchTerm)) {
-                const startIndex = extractedText.indexOf(searchTerm);
-                const endIndex = startIndex + searchTerm.length;
-        
-                const contextBefore = extractedText.substring(
-                  Math.max(startIndex - 100, 0),
-                  startIndex
-                );
-                const contextAfter = extractedText.substring(
-                  endIndex,
-                  Math.min(endIndex + 10, extractedText.length)
-                );
-        
-                const highlightedContent = `${searchTerm}${contextAfter}`;
-        
-                res.send(`Search term found:${highlightedContent}`);
-              } else {
-                res.send('Search term not found.');
-              }
+                const extractedText = data.text;
+
+                // Now you can search for specific data in the extracted text
+                const searchTerm = req.body.searchTerm; // Replace with the data you want to find
+
+                if (extractedText.includes(searchTerm)) {
+                    const startIndex = extractedText.indexOf(searchTerm);
+                    const endIndex = startIndex + searchTerm.length;
+
+                    const contextBefore = extractedText.substring(
+                        Math.max(startIndex - 100, 0),
+                        startIndex
+                    );
+                    const contextAfter = extractedText.substring(
+                        endIndex,
+                        Math.min(endIndex + 10, extractedText.length)
+                    );
+
+                    const highlightedContent = `${searchTerm}${contextAfter}`;
+
+                    res.send(`Search term found:${highlightedContent}`);
+                } else {
+                    res.send('Search term not found.');
+                }
             });
-          });
+        });
     }
-    catch(e){
-        return res.error(e.message,"Process pdf failed");
-      }
+    catch (e) {
+        return res.error(e.message, "Process pdf failed");
+    }
 }
 
-module.exports = {processPdf};
+module.exports = { processPdf };
